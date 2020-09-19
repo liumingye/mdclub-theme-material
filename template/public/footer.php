1175440541c723ead692560710e277988aa44a04
liumingye <898310895@qq.com>
2020-09-20 01:19:18 +0800

    样式修改

--- template/public/footer.php
+++ template/public/footer.php
@@ -7,7 +7,7 @@
   window.G_OPTIONS = <?= json_encode(get_options()) ?>;
 </script>
 <?php if ($NODE_ENV === 'production'): ?>
-<script src="<?= get_theme_static_url() ?>/index.f452d93e.js"></script>
+<script src="<?= get_theme_static_url() ?>/index.7e1c8e07.js"></script>
 <?php else: ?>
 <script src="http://localhost:8080/index.js"></script>
 <?php endif; ?>
